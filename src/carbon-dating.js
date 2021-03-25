c47475921d51f88451bd88d0803b2bad4d3dcd12
backspacev9 <backspacev9@gmail.com>
2021-03-26 00:49:17 +0300

    refactor: done carbone

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,15 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(x) {
+  if(!x) return false  
+  if(typeof x !== "string") return false  
+  if(!isFinite(x)) return false
+  if(Number(x) <= 0) return false
+  if(Number(x) >= 15) return false
+
+  x= Number(x);
+  let y = (Math.log(MODERN_ACTIVITY/x))/(0.693/HALF_LIFE_PERIOD); 
+  
+    return Math.ceil(y);
 };
