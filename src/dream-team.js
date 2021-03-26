ebabdbd550b7323d30c93f2e470a846c5249a74f
backspacev9 <backspacev9@gmail.com>
2021-03-26 09:51:26 +0300

    refactor: done fream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if(members == false) return false;
+  if(members == null) return false; 
+  if(members == undefined) return false; 
+  if(!Array.isArray(members)) return false; 
+  str = '';
+  arr = [];
+  members.forEach(element => {
+    if (typeof(element) == 'string'){
+      arr.push(element.trim().toUpperCase());
+    }
+    
+  });
+  members = arr.sort();
+  for(let i = 0;i < members.length;i++) {
+    if (typeof(members[i]) == 'string'){
+      str += members[i][0];
+    }
+  }
+  return str.toUpperCase();
 };
