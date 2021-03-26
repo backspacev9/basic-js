581eeb59f6a4d987f2a8dd7530c281d4f20082d0
backspacev9 <backspacev9@gmail.com>
2021-03-26 10:56:54 +0300

    refactor: done what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
+function isDate(value) {
+  switch (typeof value) {
+      case 'number':
+          return true;
+      case 'string':
+          return !isNaN(Date.parse(value));
+      case 'object':
+          if (value instanceof Date) {
+              return !isNaN(value.getTime());
+          }
+      default:
+          return false;
+  }
+}
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (!isDate(date)) return 'Error'
+  let m = date.getMonth();
+  switch(m) {
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  case 11:  
+  case 0: 
+  case 1:
+    return 'winter';
+    break;
+
+  case 2:  
+  case 3: 
+  case 4:
+    return 'spring';
+    break;
+
+  case 5:  
+  case 6: 
+  case 7:
+    return 'summer';
+    break;
+
+  case 8:  
+  case 9: 
+  case 10:
+    return 'autumn';
+    break;
+
+  default:
+    break;
+}
 };
