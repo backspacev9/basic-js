90c5e7ef9d4d17c646dbda7961ece709e1fdf092
backspacev9 <backspacev9@gmail.com>
2021-03-29 22:58:15 +0300

    gg

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let str2='';
+
+    for (let i = 0; i <options.additionRepeatTimes; i++ ){
+        str2 += options.addition +options.additionSeparator;
+    }
+   str2 = str2.slice(0,str2.length-options.additionSeparator.length)
+    for (let i = 0; i <options.repeatTimes; i++ ){
+        str = str+ str2.substr(0) + String(options.separator);
+    }
+    str  = str.slice(0,str.length-options.separator.length)
+
+    for (const [key, value] of Object.entries(options)) {
+      console.log(key+' : '+ value);
+    }
+
+    return str;
 };
   
